a6253431847280e0576aa1071fc00f7287d2fde7
doublebyte <doublebyte@hushmail.com>
2023-04-05 14:34:18 +0100

    - updated php url to avoid CORS issue

--- dist/assets/mail/contact_me.js
+++ dist/assets/mail/contact_me.js
@@ -20,7 +20,7 @@ $(function() {
       $this = $("#sendMessageButton");
       $this.prop("disabled", true); // Disable submit button until AJAX call is complete to prevent duplicate messages
       $.ajax({
-        url: "https://doublebyte.net/html/yellow-bricks/dist/assets/mail/contact_me.php",
+        url: "https://byteroad.net/html/yellow-bricks/dist/assets/mail/contact_me.php",
         type: "POST",
         data: {
           name: name,
